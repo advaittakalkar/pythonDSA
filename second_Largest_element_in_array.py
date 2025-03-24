c2cc5bf5a620f8e1ecc7528f6bd505ec6dc3e1fe
advaittakalkar <advait.takalkar@gmail.com>
2025-03-24 22:26:41 +0530

    Second largest element in an array

---
+++ second_Largest_element_in_array.py
@@ -0,0 +1,23 @@
+# Naive approach - sort first then get 2nd element  -- O(n log n)
+# Better approach - two iteration search      -- O(2*n) -> O(n)
+# Expected approach - One iteration search     -- O(n)
+
+def secondHighest(numbers):
+    l = -1
+    sl = -1
+
+    for i in numbers:
+        if i > l:
+            sl = l
+            l = i
+
+        elif i > sl and i < l:
+            sl = i
+
+    return sl
+
+
+numList = [10,10,10]
+secondLargest = secondHighest(numList)
+print(f"second largest is {secondLargest}")
+

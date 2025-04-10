d1bcdf05a9f898c6e84c9cc6a77f99a0f06c2baa
advaittakalkar <advait.takalkar@gmail.com>
2025-04-10 08:56:48 +0530

    valid parenthesis

---
+++ Leetcode/String/index_first_occurance.py
@@ -0,0 +1,19 @@
+# def indexFirstOccurance(haystack, needle):
+#     index = haystack.find(needle)
+#     print(index)
+
+def indexFirstOccurance(haystack, needle):
+    n = len(haystack)
+    m = len(needle)
+    if not needle:
+        print("no needle")
+    for i in range(0, n-m+1):
+        if haystack[i:i+m] == needle:
+            print(f"first occurances is at index {i}")
+            break
+
+
+if __name__ == "__main__":
+    haystack = "butsad"
+    needle = "sad"
+    indexFirstOccurance(haystack, needle)
